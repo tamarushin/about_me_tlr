eee53a76877c798f4d5a0d056acba2002120876c
tamarushin <tamarushin@gmail.com>
2017-10-23 15:04:54 -0700

    fixed bug

--- about_me_with_lists/app.js
+++ about_me_with_lists/app.js
@@ -4,19 +4,17 @@ var paper = prompt('does tama have a degree?');
 // var paper = paper.toUpperCase();
 
 console.log('tamas degree', paper);
-paper = paper.toLowerCase();
-if (paper === 'yes' || paper === 'y') {
+if (paper === 'yes' || paper === 'y')  {
   alert ('That\'s right!!');
 } else {
   alert ('your wrong, i do!');
 }
 
 var hobbies = prompt('does tama have hobbies?');
-// var hobbies = hobbies.toLowerCase();
+// var hobbies = hobbies.toUpperCase();
 
-console.log('TAMAS HOBBIES', hobbies);
+console.log('tamas hobbies', hobbies);
 
-hobbies = hobbies.toLowerCase();
 if (hobbies === 'yes' || hobbies === 'y') {
   alert ('i do');
 } else {
@@ -24,45 +22,37 @@ if (hobbies === 'yes' || hobbies === 'y') {
 }
 
 var animal = prompt('is tamas favorite animal a dog?');
+// var animal = animal.toUpperCase();
 
 console.log('tamas animal', animal);
-if (animal === 'yes') {
+if (animal === 'yes' || animal === 'y')  {
   alert ('grrrrrreat!');
 } else {
   alert ('your wrong, it is!');
 }
 
 var kid = prompt('does tama have a son?');
-
+// var kid = kid.toUpperCase();
 console.log('tamas son', kid);
-if (kid === 'yes') {
-  alert('nope, a daughter!');
+if (kid === 'yes' || kid === 'y')  {
+  alert('Nope, a daughter!');
 } else {
   alert('Right! I have a daughter!');
 }
-var num;
+var number
 var counter = 1;
-var maxGuess = 4;
 
-while (num !== 32) {
-  num = parseInt(prompt('What is Tamas favorite number?'));
-  console.log('tama fav number:', num);
+while (number !== 32) {
+  number = parseInt(prompt('What is Tamas favorite number?'));
 
-  if(num < 31){
+  if(number < 31){
     alert('sorry you guessed too low');
-    counter++;
-  } else if (num === 32) {
-    alert('AWESOME! You picked ' + num);
-    break;
-    counter++;
-  } else if (num > 33){
+  }else if (number > 33){
     alert('sorry you guessed too high');
-    counter++;
-  }else if (isNaN(num) || num === null) {
+  }else if (number === NaN || number === null) {
     alert('please enter a number');
-    counter++;
-  } else if (counter >= maxGuess); {
-    break;
+    counter++
   }
 }
+alert('AWSOME! You picked ' + number)
 console.log('counter:', counter);
